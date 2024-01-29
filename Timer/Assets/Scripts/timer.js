5b04eca401f247868de84b72f9cf080a884eb154
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-29 15:12:08 +0000

    Update timer.js
    Fixed issue with timer buttons still incrementing the previously pressed button

--- Timer/Assets/Scripts/timer.js
+++ Timer/Assets/Scripts/timer.js
@@ -3,45 +3,43 @@ let hr = document.getElementById('hr');
 let min = document.getElementById('min');
 let sec = document.getElementById('secs');
 let start = document.getElementById('start');
-let pauseStop = document.getElementById('pause');
+let pause = document.getElementById('pause');
 let clear = document.getElementById('clear');
 
 let numberHr = 0;
 let numberMin = 0;
 let numberSec = 0;
 
-//set timer
 display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
 
+//set timer
 hr.addEventListener('click', function increment() {
-    display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
-    
     if (numberHr >= 23) {
-        return numberHr = 0;
+        return numberHr = -1;
     }
     else {
         numberHr++
-
+        display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
     }
 });
 
 min.addEventListener('click', function increment() {
-    display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
     if (numberMin >= 59) {
-        return numberMin = 0, numberHr++;
+        return numberMin = -1, numberHr++;
     }
     else { 
         numberMin++
+        display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
     }
 });
 
 sec.addEventListener('click', function increment() {
-    display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
     if (numberSec >= 59) {
-        return numberSec = 0, numberMin++;
+        return numberSec = -1, numberMin++;
     }
     else {
         numberSec++
+        display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
     }
 });
 
@@ -51,4 +49,4 @@ clear.addEventListener('click', function clear() {
     numberMin = 0;
     numberSec = 0;
     display.innerHTML = Number(numberHr).toLocaleString('en') + ':' + Number(numberMin).toLocaleString('en') + ':' + Number(numberSec).toLocaleString('en');
-})
+});
