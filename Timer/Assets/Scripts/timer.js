d57b76af24256504620b5cce98c5e9e1fe86511d
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-29 15:39:18 +0000

    Update timer.js
    Added notes and cleaned up code

--- Timer/Assets/Scripts/timer.js
+++ Timer/Assets/Scripts/timer.js
@@ -1,3 +1,4 @@
+//gathers data from the html file.
 let display = document.getElementById('display');
 let hr = document.getElementById('hr');
 let min = document.getElementById('min');
@@ -6,16 +7,18 @@ let start = document.getElementById('start');
 let pause = document.getElementById('pause');
 let clear = document.getElementById('clear');
 
+//sets the timer variables to zero.
 let numberHr = 0;
 let numberMin = 0;
 let numberSec = 0;
 
+//This sets the timer variables to show in the timer display whilst using a min of two digits.
 function timerDisplay() {
     display.innerHTML = Number(numberHr).toLocaleString('en', { minimumIntegerDigits: 2, useGrouping: false }) + ':' + Number(numberMin).toLocaleString('en', { minimumIntegerDigits: 2, useGrouping: false }) + ':' + Number(numberSec).toLocaleString('en', { minimumIntegerDigits: 2, useGrouping: false });
 }
 timerDisplay();
 
-//set timer
+//sets timer variables using buttons
 hr.addEventListener('click', function increment() {
     if (numberHr >= 23) {
         return numberHr = -1;
@@ -46,8 +49,8 @@ sec.addEventListener('click', function increment() {
     }
 });
 
+//resets timer variables and clears the display 
 clear.addEventListener('click', function clear() {
-    display.innerHTML = "";
     numberHr = 0;
     numberMin = 0;
     numberSec = 0;
